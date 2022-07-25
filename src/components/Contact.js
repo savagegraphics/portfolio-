29af9739ad88760a587d4ad1d6ade3107e429fd8
savagegraphics <olawaleraji188@gmail.com>
2022-07-25 08:02:46 -0500

    main

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,99 +1,107 @@
-import React, { useRef } from 'react';
-// import emailjs from '@emailjs/browser';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Navbar from './Navbar';
+import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
+import "./Contact.css";
 
+export default function Contact() {
+    const [formData, setFormData] = useState({
+        from_name: "",
+        reply_to: "",
+        message: ""
+    });
+    const { from_name, reply_to, message } = formData;
 
-//                 const form = useRef();
+    const onChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        });
+    };
 
+    const onSubmit = (e) => {
+        e.preventDefault();
 
-//   const sendEmail = (e) => (
-//   {
-//     e.preventDefault();
+        formData["to_name"] = "yourname";
 
-//     emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-//   });
+        const YOUR_SERVICE_ID = "service_ID";
+        const YOUR_TEMPLATE_ID = "template_ID";
+        const YOUR_PUBLIC_KEY = "public-Key";
 
+        // Sending the mail
+        emailjs
+            .send(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, formData, YOUR_PUBLIC_KEY)
+            .then(
+                () => {
+                    console.log("Email Sent");
+                },
+                (error) => {
+                    console.log(JSON.stringify(error));
+                }
+            );
 
-function Contact() {
+        console.log(formData);
+
+        setFormData({
+            from_name: "",
+            reply_to: "",
+            message: ""
+        })
+    };
+
     return (
-        <div>
-            < Navbar />
+        <div className='form-body'>
 
-            <div className="content-contact">
 
-                <form>
-
-                    <h2>CONTACT US</h2>
-                    <div className='Contact'>
-
-                        <TextField
-                            helperText="Please enter your name"
-                            id="name-input"
-                            label="Name"
+            <div className="contact-container">
+                <h1>Email US</h1>
+                <form className="form" onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <input
+                            className="form-control"
+                            type="text"
+                            name="from_name"
+                            value={from_name}
+                            onChange={onChange}
+                            id="from_name"
+                            placeholder="Enter Fullname"
                         />
                     </div>
-
-                    <div className='contact'>
-                        <TextField
-                            id="email"
-                            label="Email"
-                            type="email"
-                            autoComplete="current-email"
-                            variant="filled"
+                    <div className="form-group">
+                        <input
+                            className="form-control"
+                            type="text"
+                            name="reply_to"
+                            value={reply_to}
+                            onChange={onChange}
+                            id="reply_to"
+                            placeholder="Enter Email"
                         />
                     </div>
-
-
-                    <div className='contact'>
-                        <TextField
-                            id="outlined-textarea"
-                            label="Message"
-                            placeholder="Message"
-                            multiline
-                            rows={2}
-                        />
+                    <div className="form-group">
+                        <textarea
+                            className="form-control"
+                            name="message"
+                            value={message}
+                            onChange={onChange}
+                            id="message"
+                            placeholder="Enter Message"
+                        ></textarea>
                     </div>
+                    <button type="submit" id="button" className="btn btn-block">
+                        Submit
+                    </button>
 
-                    <div>
 
 
-                        <Button className='contact' variant="outlined">Submit</Button>
-
+                    <div className='mail-us'>
+                        <span class="fa fa-phone"></span> 📞 +27 78 268 7290
+                        <span class="fa fa-envelope-o"></span> info@residencefilms.com   📧
                     </div>
-
 
                 </form>
-
-                <div className='mail-us'>
-                    <span class="fa fa-phone"></span> 📞 +27 78 268 7290
-                    <span class="fa fa-envelope-o"></span> info@residencefilms.com   📧
-                </div>
-
             </div>
         </div>
+
     );
 }
 
 
-//  {
-//     return (
-//         <div className='content-contact'>
-//             <Navbar />
-//             <form className="form">
-//                 <h2>CONTACT US</h2>
-//                 <p type="Name:"><input placeholder="Write your name here.."></input></p>
-//                 <p type="Email:"><input placeholder="Let us know how to contact you back.."></input></p>
-//                 <p type="Message:"><input placeholder="What would you like to tell us.."></input></p>
-//                 <button>Send Message</button>
-//                 <div className='mail-us'>
-//                     <span class="fa fa-phone"></span>  +27 78 268 7290
-//                     <span class="fa fa-envelope-o"></span> info@residencefilms.com
-//                 </div>
-//             </form>
-//         </div>
-//     )
-// }
-
-export default Contact
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,55 +1,29 @@
 import React from 'react'
+import './About.css'
 
 function About() {
     return (
-        <div className='about-us'>
 
-
-            <div class="content is-medium">
-                <h2>ABOUT US</h2>
+        <div className='about'>
+            <div className='about-us'>
+                <h1 className='about-title'>
+                    About US
+                </h1>
                 <p>
                     Residence Films is a creative studio, passionate about telling stories through visual art and moving pictures. Our pool of skilled and competent production specialists manage every area of the production process with extreme dedication to execute briefs, achieve the desired results, and produce great content for our clients and their audiences.
                 </p>
+
+                <p>
+
+                    Our Production team enhances footage with special effects, transitions, music, and sound to captivate audiences and elevate the quality of the content. Our services range from Film and Television Production, Offline and Online Edit for Branded Content, Short form Content, Digital Marketing, Music Video production, sound design, color grade and VFX.
+
+                </p>
+
+                <p className='quote'> "Telling stories through visual art and moving picture"..</p>
+
             </div>
-
-            <div class="box">
-                <article class="media">
-                    <div class="media-left">
-                        <figure class="image is-64x64">
-                        </figure>
-                    </div>
-                    <div class="media-content">
-                        <div class="content">
-                            <p>
-
-                                Our Production team enhances footage with special effects, transitions, music, and sound to captivate audiences and elevate the quality of the content. Our services range from Film and Television Production, Offline and Online Edit for Branded Content, Short form Content, Digital Marketing, Music Video production, sound design, color grade and VFX.
-
-                            </p>
-                        </div>
-                        <nav class="level is-mobile">
-                            <div class="level-left">
-                                <a class="level-item" aria-label="reply">
-                                    <span class="icon is-small">
-                                        <i class="fas fa-reply" aria-hidden="true"></i>
-                                    </span>
-                                </a>
-                                <a class="level-item" aria-label="retweet">
-                                    <span class="icon is-small">
-                                        <i class="fas fa-retweet" aria-hidden="true"></i>
-                                    </span>
-                                </a>
-                                <a class="level-item" aria-label="like">
-                                    <span class="icon is-small">
-                                        <i class="fas fa-heart" aria-hidden="true"></i>
-                                    </span>
-                                </a>
-                            </div>
-                        </nav>
-                    </div>
-                </article>
-            </div>
-
         </div>
+
     )
 }
 
